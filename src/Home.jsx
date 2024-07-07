dfeaf2badbea3a17d96408e9a55ca8015292e0ef
namansingla-coder <namansingla@myyahoo.com>
2024-07-08 00:49:38 +0530

    Fff

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 function Home(){
     return (
-        <div className="flex h-screen justify-center">
+        <div className="flex h-screen ">
             <button className="self-center border px-4 rounded-md bg-blue-600 py-1"><Link to="/login">Login</Link></button>
         </div>
     )
