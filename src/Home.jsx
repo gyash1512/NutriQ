c66677feeb5b9b8cc6bbb89d2666ca4989890048
gyash1512 <gyash.1512@gmail.com>
2024-07-07 23:47:38 +0530

    commit

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Link} from "react-router-dom";
+function Home(){
+    return (
+        <div className="flex h-screen justify-center">
+            <button className="self-center border px-4 rounded-md bg-blue-600 py-1"><Link to="/login">Login</Link></button>
+        </div>
+    )
+}
+export default Home;
